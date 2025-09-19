385f0c6a818cfb0076eb228c0e9926f642498a03
IsaacLusca <isaaclucas.bezerra@gmail.com>
2025-09-19 13:51:34 -0300

    removendo classe

--- main.py
+++ main.py
@@ -6,11 +6,4 @@ print("4.Marcar Evento como Participado")
 print("5.Gerar Relatório")
 print("6.Sair")
 
-# Criando a classe Evento
-class Evento:
-    def __init___(self, nome, data, local, categoria):
-        self.nome = nome
-        self.data = data
-        self.local = local
-        self.categoria = categoria
-        self.participado = False
+
